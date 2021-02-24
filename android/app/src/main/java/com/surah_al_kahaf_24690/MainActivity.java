21706cf74ef2eb687ac9769e439fa796769d56d8
Crowdbotics <team@crowdbotics.com>
2021-02-24 03:39:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/surah_al_kahaf_24690/MainActivity.java
@@ -0,0 +1,15 @@
+package com.surah_al_kahaf_24690;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "surah_al_kahaf_24690";
+    }
+}
